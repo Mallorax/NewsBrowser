af742699822b3196703c8e57b9d384376ceaddae
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-11-29 17:31:08 +0100

    Creating first dependency injection and retrofit interface

---
+++ app/src/main/java/com/example/newsbrowser/hilt/ApplicationModule.kt
@@ -0,0 +1,27 @@
+package com.example.newsbrowser.hilt
+
+import com.example.newsbrowser.rest.BreakingNewsRetrofitEndpoint
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): BreakingNewsRetrofitEndpoint {
+        return Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(BreakingNewsRetrofitEndpoint::class.java)
+
+    }
+}
---
+++ app/src/main/java/com/example/newsbrowser/rest/BreakingNewsRetrofitEndpoint.kt
@@ -0,0 +1,4 @@
+package com.example.newsbrowser.rest
+
+interface BreakingNewsRetrofitEndpoint {
+}
