940e291171104eaa529fde7bb33e33efda4ebf95
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-12-01 18:22:13 +0100

    Navigation setup

--- app/src/main/java/com/example/newsbrowser/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsbrowser/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.newsbrowser.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsbrowser.R
+import com.example.newsbrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindig: ActivityMainBinding
+    private val navController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindig = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navView = bindig.navView
+        navView.setupWithNavController(navController)
+        val appBarConfig = AppBarConfiguration(navController.graph, bindig.drawerLayout)
+        setSupportActionBar(bindig.appToolbar)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(navController, bindig.drawerLayout)
     }
 }
--- app/src/main/java/com/example/newsbrowser/ui/breaking/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/newsbrowser/ui/breaking/BreakingNewsFragment.kt
@@ -1,13 +1,26 @@
 package com.example.newsbrowser.ui.breaking
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.newsbrowser.databinding.BreakingNewsFragmentBinding
 
 class BreakingNewsFragment: Fragment() {
 
+    private var _binding: BreakingNewsFragmentBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = BreakingNewsFragmentBinding.inflate(inflater)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 }
