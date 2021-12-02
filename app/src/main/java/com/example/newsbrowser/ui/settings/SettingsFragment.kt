75eafc2ff06f9b45615dd2fae28b8f4667e80c33
DESKTOP-Mallorax <patryk733773@gmail.com>
2021-12-02 20:18:38 +0100

    Added id's to views

--- app/src/main/java/com/example/newsbrowser/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/newsbrowser/ui/settings/SettingsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.newsbrowser.databinding.BreakingNewsFragmentBinding
 import com.example.newsbrowser.databinding.SettingsFragmentBinding
 
 class SettingsFragment: Fragment() {
